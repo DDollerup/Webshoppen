2974e0cfd1e29d4c06987d2e9819e766d66fe5dd
PDDaniel <daniel@proper-dog.com>
2017-01-23 08:59:39 +0100

    Add project files.

---
+++ Webshoppen/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Webshoppen.Models;
+using Webshoppen.Factories;
+
+namespace Webshoppen.Controllers
+{
+    public class HomeController : Controller
+    {
+        ProductFactory productFac = new ProductFactory();
+        CategoryFactory categoryFac = new CategoryFactory();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            ViewBag.Categories = categoryFac.GetAll();
+            List<Product> products = TempData["SortedProducts"] as List<Product>;
+            if (products == null)
+            {
+                List<Product> allProducts = productFac.GetAll();
+                return View(allProducts);
+            }
+            else
+            {
+                return View(products);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult SortProducts(int id = 0)
+        {
+            List<Product> sortedProducts = null;
+            if (id > 0)
+            {
+                sortedProducts = productFac.GetBy("CategoryID", id);
+            }
+
+            TempData["SortedProducts"] = sortedProducts;
+            return RedirectToAction("Products");
+        }
+
+        public ActionResult ShowProduct(int id = 0)
+        {
+            Product productToView = productFac.Get(id);
+
+            ViewBag.Category = categoryFac.Get(productToView.CategoryID);
+
+            return View(productToView);
+        }
+    }
+}
---
+++ Webshoppen/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webshoppen.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+            Image = "no-image.jpg";
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Price { get; set; }
+        public bool OnSale { get; set; } // true false
+        public string SalePrice { get; set; }
+        public int CategoryID { get; set; }
+    }
+}

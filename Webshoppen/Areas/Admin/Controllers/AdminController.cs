bb274af672cd2adbb22d0724e617d5bdcdc37088
PDDaniel <daniel@proper-dog.com>
2017-01-25 14:08:26 +0100

    Added Delete and Edit product to admin area.

--- Webshoppen/Areas/Admin/Controllers/AdminController.cs
+++ Webshoppen/Areas/Admin/Controllers/AdminController.cs
@@ -11,6 +11,9 @@ namespace Webshoppen.Areas.Admin.Controllers
     public class AdminController : Controller
     {
         HomeFactory homeFac = new HomeFactory();
+        ContactFactory contactFac = new ContactFactory();
+        ProductFactory productFac = new ProductFactory();
+        CategoryFactory categoryFac = new CategoryFactory();
 
         public ActionResult Index()
         {
@@ -24,6 +27,50 @@ namespace Webshoppen.Areas.Admin.Controllers
             homeFac.Update(home);
             TempData["MSG"] = "Home has been updated.";
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Contact()
+        {
+            Contact contact = contactFac.Get(1);
+            return View(contact);
+        }
+
+        [HttpPost]
+        public ActionResult ContactSubmit(Contact contact)
+        {
+            contactFac.Update(contact);
+            TempData["MSG"] = "Contact has been updated.";
+            return RedirectToAction("Contact");
+        }
+
+        public ActionResult Products()
+        {
+            List<Product> allProducts = productFac.GetAll();
+            return View(allProducts);
+        }
+
+        /* Manage Products */
+
+        public ActionResult DeleteProduct(int id = 0)
+        {
+            productFac.Delete(id);
+            TempData["MSG"] = "Product with id: " + id + " has been deleted.";
+            return RedirectToAction("Products");
+        }
+
+        public ActionResult EditProduct(int id = 0)
+        {
+            ViewBag.Categories = categoryFac.GetAll();
+            Product productToEdit = productFac.Get(id);
+            return View(productToEdit);
+        }
+
+        [HttpPost]
+        public ActionResult EditProductSubmit(Product product)
+        {
+            productFac.Update(product);
+            TempData["MSG"] = "Product: " + product.Name + " has been updated.";
+            return RedirectToAction("Products");
         }
     }
 }

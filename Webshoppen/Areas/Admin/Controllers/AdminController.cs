b8b7f4731ece552b7805cc3f477ef0499181b6e9
PDDaniel <daniel@proper-dog.com>
2017-01-26 14:30:06 +0100

    Added FileUpload, Create Product and Login to Admin

--- Webshoppen/Areas/Admin/Controllers/AdminController.cs
+++ Webshoppen/Areas/Admin/Controllers/AdminController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using Webshoppen.Factories;
@@ -10,10 +12,21 @@ namespace Webshoppen.Areas.Admin.Controllers
 {
     public class AdminController : Controller
     {
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (Session["Member"] == null && Request.RawUrl.ToLower().Contains("login") == false)
+            {
+                Response.Redirect("/Admin/Admin/Login");
+            }
+            base.OnActionExecuting(filterContext);
+        }
+
         HomeFactory homeFac = new HomeFactory();
         ContactFactory contactFac = new ContactFactory();
         ProductFactory productFac = new ProductFactory();
         CategoryFactory categoryFac = new CategoryFactory();
+        MemberFactory memberFac = new MemberFactory();
 
         public ActionResult Index()
         {
@@ -51,6 +64,32 @@ namespace Webshoppen.Areas.Admin.Controllers
 
         /* Manage Products */
 
+        public ActionResult CreateProduct()
+        {
+            ViewBag.Categories = categoryFac.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateProductSubmit(Product product, HttpPostedFileBase ImageFile)
+        {
+
+            if (ImageFile != null && ImageFile.ContentLength > 0)
+            {
+                string fileName = ImageFile.FileName;
+                string path = Request.PhysicalApplicationPath + @"\Content\Images\Products\" + fileName;
+                ImageFile.SaveAs(path);
+                product.Image = fileName;
+            }
+
+            TempData["MSG"] = "A product: " + product.Name + " has been added.";
+
+            // DDMF = productFac.Insert(product);
+            productFac.Add(product);
+
+            return RedirectToAction("Products");
+        }
+
         public ActionResult DeleteProduct(int id = 0)
         {
             productFac.Delete(id);
@@ -66,11 +105,52 @@ namespace Webshoppen.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        public ActionResult EditProductSubmit(Product product)
+        public ActionResult EditProductSubmit(Product product, HttpPostedFileBase ImageFile)
         {
+
+            if (ImageFile != null && ImageFile.ContentLength > 0)
+            {
+                string fileName = ImageFile.FileName;
+                string path = Request.PhysicalApplicationPath + @"\Content\Images\Products\" + fileName;
+                ImageFile.SaveAs(path);
+                product.Image = fileName;
+            }
+
             productFac.Update(product);
             TempData["MSG"] = "Product: " + product.Name + " has been updated.";
             return RedirectToAction("Products");
+        }
+
+
+        /* Manage Login */
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoginSubmit(string username, string password)
+        {
+            SHA512 encryptedPassword = new SHA512Managed();
+            encryptedPassword.ComputeHash(Encoding.ASCII.GetBytes(password));
+
+            string hashedPassword = BitConverter.ToString(encryptedPassword.Hash).Replace("-", "");
+
+            // DDMF: Member memberToLogin = MemberFactory.ExecuteSQL<Member>("")[0];
+            Member memberToLogin = memberFac.SqlQuery("SELECT * FROM Member WHERE Username='" + username +
+                                                      "' AND Password='" + hashedPassword + "'");
+
+            if (memberToLogin.ID > 0 && memberToLogin.Admin == true)
+            {
+                Session["Member"] = memberToLogin;
+            }
+            else
+            {
+                TempData["MSG"] = "Wrong username or password.";
+                return RedirectToAction("Login");
+            }
+            return RedirectToAction("Index");
         }
     }
 }
--- Webshoppen/Models/Product.cs
+++ Webshoppen/Models/Product.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Webshoppen.Models
 {
@@ -14,6 +15,7 @@ namespace Webshoppen.Models
 
         public int ID { get; set; }
         public string Name { get; set; }
+        [AllowHtml]
         public string Description { get; set; }
         public string Image { get; set; }
         public string Price { get; set; }
